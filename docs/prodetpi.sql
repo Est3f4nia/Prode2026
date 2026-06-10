e161ade875a2874add003aae86a9c35d2abebfb5
EfrainCruzTurrin <efraincruzturrin@gmail.com>
2026-06-10 10:42:01 -0300

    Actualizacion de DER

--- docs/prodetpi.sql
+++ docs/prodetpi.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `prodeprogiv` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `prodeprogiv`;
+CREATE DATABASE  IF NOT EXISTS `prodetpi` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `prodetpi`;
 -- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
 --
--- Host: localhost    Database: prodeprogiv
+-- Host: localhost    Database: prodetpi
 -- ------------------------------------------------------
 -- Server version	8.0.46
 
@@ -26,43 +26,79 @@ DROP TABLE IF EXISTS `equipo`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `equipo` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `pais` varchar(50) DEFAULT NULL,
+  `escudo_url` varchar(150) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nombre` (`nombre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `fecha_jornada`
+-- Dumping data for table `equipo`
 --
 
-DROP TABLE IF EXISTS `fecha_jornada`;
+LOCK TABLES `equipo` WRITE;
+/*!40000 ALTER TABLE `equipo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `equipo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fecha`
+--
+
+DROP TABLE IF EXISTS `fecha`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `fecha_jornada` (
+CREATE TABLE `fecha` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) NOT NULL,
-  `estado` enum('PROGRAMADA','EN_JUEGO','FINALIZADA') NOT NULL DEFAULT 'PROGRAMADA',
+  `nombre` varchar(50) NOT NULL,
+  `estado` enum('PROGRAMADA','EN_JUEGO','FINALIZADO') NOT NULL DEFAULT 'PROGRAMADA',
   PRIMARY KEY (`id`),
   UNIQUE KEY `nombre` (`nombre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `grupo`
+-- Dumping data for table `fecha`
 --
 
-DROP TABLE IF EXISTS `grupo`;
+LOCK TABLES `fecha` WRITE;
+/*!40000 ALTER TABLE `fecha` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fecha` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `historial_equipo`
+--
+
+DROP TABLE IF EXISTS `historial_equipo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `grupo` (
+CREATE TABLE `historial_equipo` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) NOT NULL,
-  `codigo_invitacion` varchar(50) NOT NULL,
+  `equipo_id` bigint NOT NULL,
+  `partidos_jugados` int DEFAULT '0',
+  `victorias` int DEFAULT '0',
+  `empates` int DEFAULT '0',
+  `derrotas` int DEFAULT '0',
+  `goles_favor` int DEFAULT '0',
+  `goles_contra` int DEFAULT '0',
+  `ultimos_resultados` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `codigo_invitacion` (`codigo_invitacion`)
+  UNIQUE KEY `unq_equipo_id` (`equipo_id`),
+  CONSTRAINT `fk_historial_equipo` FOREIGN KEY (`equipo_id`) REFERENCES `equipo` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `historial_equipo`
+--
+
+LOCK TABLES `historial_equipo` WRITE;
+/*!40000 ALTER TABLE `historial_equipo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `historial_equipo` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `partido`
@@ -73,24 +109,33 @@ DROP TABLE IF EXISTS `partido`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `partido` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `fecha_jornada_id` bigint NOT NULL,
+  `fecha_id` bigint NOT NULL,
   `equipo_local_id` bigint NOT NULL,
   `equipo_visitante_id` bigint NOT NULL,
   `fecha_hora_inicio` datetime NOT NULL,
   `estado` enum('POR_JUGARSE','EN_JUEGO','FINALIZADO') NOT NULL DEFAULT 'POR_JUGARSE',
-  `goles_local_real` int DEFAULT NULL,
-  `goles_visitante_real` int DEFAULT NULL,
-  `tendencia_real` enum('LOCAL','EMPATE','VISITANTE') DEFAULT NULL,
+  `goles_local` int DEFAULT NULL,
+  `goles_visitante` int DEFAULT NULL,
+  `resultado` enum('LOCAL','EMPATE','VISITANTE') DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `fecha_jornada_id` (`fecha_jornada_id`),
+  KEY `fecha_id` (`fecha_id`),
   KEY `equipo_local_id` (`equipo_local_id`),
   KEY `equipo_visitante_id` (`equipo_visitante_id`),
-  CONSTRAINT `partido_ibfk_1` FOREIGN KEY (`fecha_jornada_id`) REFERENCES `fecha_jornada` (`id`),
-  CONSTRAINT `partido_ibfk_2` FOREIGN KEY (`equipo_local_id`) REFERENCES `equipo` (`id`),
-  CONSTRAINT `partido_ibfk_3` FOREIGN KEY (`equipo_visitante_id`) REFERENCES `equipo` (`id`),
+  CONSTRAINT `fk_partido_fecha` FOREIGN KEY (`fecha_id`) REFERENCES `fecha` (`id`),
+  CONSTRAINT `fk_partido_local` FOREIGN KEY (`equipo_local_id`) REFERENCES `equipo` (`id`),
+  CONSTRAINT `fk_partido_visitante` FOREIGN KEY (`equipo_visitante_id`) REFERENCES `equipo` (`id`),
   CONSTRAINT `chk_equipos_distintos` CHECK ((`equipo_local_id` <> `equipo_visitante_id`))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `partido`
+--
+
+LOCK TABLES `partido` WRITE;
+/*!40000 ALTER TABLE `partido` DISABLE KEYS */;
+/*!40000 ALTER TABLE `partido` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `pronostico`
@@ -103,17 +148,26 @@ CREATE TABLE `pronostico` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `usuario_id` bigint NOT NULL,
   `partido_id` bigint NOT NULL,
-  `goles_local` int NOT NULL,
-  `goles_visitante` int NOT NULL,
-  `fecha_carga` datetime NOT NULL,
-  `puntos_obtenidos` int NOT NULL DEFAULT '0',
+  `goles_local_predicho` int NOT NULL,
+  `goles_visitante_predicho` int NOT NULL,
+  `puntos_otorgados` int NOT NULL DEFAULT '0',
+  `fecha_creacion` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unq_usuario_partido` (`usuario_id`,`partido_id`),
   KEY `partido_id` (`partido_id`),
-  CONSTRAINT `pronostico_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`),
-  CONSTRAINT `pronostico_ibfk_2` FOREIGN KEY (`partido_id`) REFERENCES `partido` (`id`)
+  CONSTRAINT `fk_pronostico_partido` FOREIGN KEY (`partido_id`) REFERENCES `partido` (`id`),
+  CONSTRAINT `fk_pronostico_usuario` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pronostico`
+--
+
+LOCK TABLES `pronostico` WRITE;
+/*!40000 ALTER TABLE `pronostico` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pronostico` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `usuario`
@@ -125,11 +179,11 @@ DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE `usuario` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password_hash` varchar(255) NOT NULL,
-  `rol` enum('USER','ADMIN') NOT NULL DEFAULT 'USER',
+  `email` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `rol` enum('ADMIN','JUGADOR') NOT NULL DEFAULT 'JUGADOR',
   `puntos_totales` int NOT NULL DEFAULT '0',
-  `plenos_acertados` int NOT NULL DEFAULT '0',
+  `resultados_exactos` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
@@ -137,21 +191,13 @@ CREATE TABLE `usuario` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `usuario_grupo`
+-- Dumping data for table `usuario`
 --
 
-DROP TABLE IF EXISTS `usuario_grupo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usuario_grupo` (
-  `usuario_id` bigint NOT NULL,
-  `grupo_id` bigint NOT NULL,
-  PRIMARY KEY (`usuario_id`,`grupo_id`),
-  KEY `grupo_id` (`grupo_id`),
-  CONSTRAINT `usuario_grupo_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `usuario_grupo_ibfk_2` FOREIGN KEY (`grupo_id`) REFERENCES `grupo` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -162,4 +208,4 @@ CREATE TABLE `usuario_grupo` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-06-08 13:40:00
+-- Dump completed on 2026-06-09 12:00:48
